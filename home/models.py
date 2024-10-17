21b02640023cf1cd969ba51effd9a70b2c4584fa
SyaminiSyukri <23FTT1101@student.pb.edu.bn>
2024-10-17 09:27:35 +0800

    updating

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    sname = models.CharField(max_length=150)
+    tags = models.TextField()
+    sdesc = models.TextField()
+    ldesc = models.TextField()
+    img = models.FileField(upload_to="pic/%y/")
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering=('id',)
+    
+    def get_absolute_url(self):
+        return reverse('details',args=[self.id,self.slug])
--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, HttpResponse
+from .models import Image
+from django.db.models import Q
 
 # Create your views here.
+def index(request):
+    obj=Image.objects.all()
+    return render(request, 'home/index.html',{'image':obj})
+
+def detail_view(request, id, slug):
+    obj=get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 'home/detail.html',{'image':obj})
+
+def search(request):
+    query=None
+    result=[]
+    if request.method == 'GET' :
+        query=request.GET.get('search')
+        result=Image.objects.filter(Q(title__icontains=query) | Q(sname__icontains=query) | Q(tags__icontains=query)
+                                    | Q(sdesc__icontains=query) | Q(ldesc__icontains=query))
+    return render(request, 'home/search.html',{'query':query, 'result':result})
