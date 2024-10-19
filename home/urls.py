68c8e5e7038222e6d5aeccc112abda8c0abe14b2
SyaminiSyukri <23FTT1101@student.pb.edu.bn>
2024-10-19 09:13:47 +0800

    updating

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('', views.index, name='index'),
     path('<id>/<str:slug>/', views.detail_view, name='details'),
     path('search/', views.search, name='search'),
+
+    path('notes/', views.notesPage, name='notes'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.shortcuts import get_object_or_404, render, HttpResponse, redirect
 from .models import Image
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = UserCreationForm()
+
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'home/register.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username OR Password is incorrect')
+
+    context = {}
+    return render(request, 'home/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def index(request):
     obj=Image.objects.all()
     return render(request, 'home/index.html',{'image':obj})
 
+@login_required(login_url='login')
 def detail_view(request, id, slug):
     obj=get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'home/detail.html',{'image':obj})
 
+@login_required(login_url='login')
 def search(request):
     query=None
     result=[]
@@ -19,3 +68,8 @@ def search(request):
         result=Image.objects.filter(Q(title__icontains=query) | Q(sname__icontains=query) | Q(tags__icontains=query)
                                     | Q(sdesc__icontains=query) | Q(ldesc__icontains=query))
     return render(request, 'home/search.html',{'query':query, 'result':result})
+
+
+def notesPage(request):
+    context = {}
+    return render(request, 'home/notes.html', context)
